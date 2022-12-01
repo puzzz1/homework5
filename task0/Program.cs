04f7ea9b04a69be8835ee91edb76d12f5aa5b652
puzz <taste@inbox.ru>
2022-12-01 13:35:55 +0300

    make homework

---
+++ task0/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+
+int[] GenerateRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+
+}
+
+int FindNumberInArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.Write("В этом массиве - ");
+int[] array = GenerateRandomArray(4, 100, 999);
+printArray(array);
+
+int count = FindNumberInArray(array);
+Console.WriteLine($" {count} четных элементa");
---
+++ task1/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный 
+случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+
+int[] GenerateRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+
+}
+
+int FindNumberInArray(int[] array)
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)
+        {
+           index = index + array[i];
+        }
+    }
+    return index;
+}
+Console.Write("В этом массиве - ");
+int[] array = GenerateRandomArray(5, -10, 10);
+printArray(array);
+
+int index = FindNumberInArray(array);
+Console.WriteLine($" -  {index} сумма нечетных элементов");
